40b1ec5ac2dbbeb3991fdda13d8a93e299ed99a3
Matthew Kidza-Griffiths <mrmatthewgriffiths@gmail.com>
2010-07-08 11:45:16 +0100

    removed the testing configuration from the SiLCC Pre Processing Step

--- core/Modules/SiLCCInterface/SiLCCPreProcessingStep.php
+++ core/Modules/SiLCCInterface/SiLCCPreProcessingStep.php
@@ -78,12 +78,7 @@ class SiLCCPreProcessingStep implements \Swiftriver\Core\PreProcessing\IPreProce
         return $taggedContentItems;
     }
     public function ReturnRequiredParameters() {
-        return array(
-            new \Swiftriver\Core\ObjectModel\ConfigurationElement(
-                    "Url", 
-                    "string", 
-                    "The fully qulaified URL of the SiLCC Service")
-        );
+        return array();
     }
 }
 ?>
