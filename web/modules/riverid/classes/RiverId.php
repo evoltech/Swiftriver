1df3e3943326c7b67b92d0062ba1036833767b71
Matthew Kidza-Griffiths <mrmatthewgriffiths@gmail.com>
2010-07-15 10:58:12 +0100

    Initial checkin of the basic RiverId kohana plugin

--- web/modules/riverid/classes/RiverId.php
+++ web/modules/riverid/classes/RiverId.php
@@ -1,8 +1,78 @@
 <?php
 class RiverId {
-    public function is_logged_in()
+    /**
+     * This function checks that the current user is logged in.
+     * The return value is an associative array in the following
+     * format:
+     * array(
+     *  "IsLoggedIn" => bool,
+     *  "Role" => string
+     * );
+     * @return array(bool,role)
+     */
+    public static function is_logged_in()
     {
+        $key = $_SESSION["authkey"];
 
+        $username = $_SESSION["username"];
+
+        if($key == null || $key == "" || $username == null || $username == "") 
+            return array("IsLoggedIn" => false);
+
+        $result = self::get_hashes_password_and_role($username);
+
+        if($result === false)
+            return array("IsLoggedIn" => false);;
+
+        $password = $result["password"];
+
+        if($password != $key)
+            return array("IsLoggedIn" => false);
+
+        $role = $result["role"];
+
+        $return = array(
+            "IsLoggedIn" => true,
+            "Role" => $role
+        );
+
+        return $return;
+    }
+
+    private static function get_hashes_password_and_role($username)
+    {
+        $con = mysql_connect(
+                Config_RiverId::$databaseurl,
+                Config_RiverId::$username,
+                Config_RiverId::$password);
+
+        mysql_select_db(Config_RiverId::$database, $con);
+
+        mysql_query(Config_RiverId::$createsql, $con);
+
+        $username = mysql_escape_string($username);
+
+        $sql = "SELECT * FROM users WHERE username = '" . $username . '";';
+
+        $results = mysql_query($sql);
+
+        $row = mysql_fetch_assoc($results);
+
+        if($row == false)
+            return false;
+
+        $role = $row["role"];
+
+        $password = $row["password"];
+
+        $hashedPassword = md5($password);
+
+        $return = array(
+            "password" => $hashedPassword,
+            "role" => $role
+        );
+
+        return $return;
     }
 }
 ?>
---
+++ web/modules/riverid/config/config.php
@@ -0,0 +1,9 @@
+<?php
+class Config_RiverId {
+    public static $databaseurl = "localhost";
+    public static $username = "swiftriver";
+    public static $password = "swiftriver";
+    public static $database = "swiftriver";
+
+    public static $createsql = "CREATE TABLE IF NOT EXISTS users ( username VARCHAR(2000), password VARCHAR(2000), role VARCHAR(2000) ) TYPE=innodb";
+}
