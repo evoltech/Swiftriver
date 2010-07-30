1aa94b28d95e188bb26a77fb255cba7bcba3ed78
Matthew Kidza-Griffiths <mrmatthewgriffiths@gmail.com>
2010-07-30 20:19:28 +0100

    changed capitalisation

--- web/modules/riverid/classes/RiverIdConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-class RiverIdConfig {
-    public static $databaseurl = "localhost";
-    public static $username = "swiftriver";
-    public static $password = "swiftriver";
-    public static $database = "swiftriver";
-
-    public static $createsql = "CREATE TABLE IF NOT EXISTS users ( username VARCHAR(2000), password VARCHAR(2000), role VARCHAR(2000) ) TYPE=innodb";
-}
