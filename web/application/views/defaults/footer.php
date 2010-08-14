3b6204776454da3eabb622519132a6cab78d0388
jongos <j.gosier@appfrica.org>
2010-08-14 19:35:54 +0300

    update readme, footer links

--- web/application/views/defaults/footer.php
+++ web/application/views/defaults/footer.php
@@ -2,15 +2,12 @@
 <div id="footer">
 <p><ul>
 	<li><a href="#">Home</a> </li>
-	<li>| <a href="#">Suggest Feeds</a> </li>
-	<li>| <a href="#">About</a> </li>
-	<li>| <a href="#">Contact</a> </li>
-	<li>| <a href="http://blog.ushahidi.com/index.php/category/swift-river/">Blog</a> </li>
-	<li>| <a href="http://groups.google.com/group/swiftriver">Support</a> </li>
-	<li>| <a href="http://swift.ushahidi.com/extend/">Extend</a> </li>
+	<li>| <a href="http://blog.swiftly.org">Swiftly Blog</a> </li>
+	<li>| <a href="http://groups.google.com/group/swiftriver">Discussion</a> </li>
+	<li>| <a href="http://swiftly.org/products-2/add-ons/">Extend</a> </li>
 	<li>| <a href="http://github.com/ushahidi/Swiftriver">Github</a> </li>
-	<li>| <a href="http://swift.ushahidi.com/doc/">Documentation</a> </li>
+	<li>| <a href="http://swiftly.org/overview/documentation/">Documentation</a> </li>
 </ul>
 </p>
-<p>&copy; 2010 <a href="http://github.com/ushahidi/Swiftriver/downloads">SwiftRiver Version 0.2.0 Batuque</a> released under the <a href="http://fsf.org/">GNU Lesser General Public License.</a></p>
+<p>&copy; 2010 <a href="http://swiftly.org">SwiftRiver v0.5.0</a> released under the <a href="http://fsf.org/">GNU Lesser General Public License.</a></p>
 </div>
