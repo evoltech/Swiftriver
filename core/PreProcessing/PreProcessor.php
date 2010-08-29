65c3054ba77aa7dd2fb0f06fceed767cf4bf9288
Matthew Kidza-Griffiths <mrmatthewgriffiths@gmail.com>
2010-08-29 15:32:24 +0100

    Documentation

--- core/PreProcessing/PreProcessor.php
+++ core/PreProcessing/PreProcessor.php
@@ -1,10 +1,27 @@
 <?php
 namespace Swiftriver\Core\PreProcessing;
-class PreProcessor {
+/**
+ * Class that manages the process of passing all content 
+ * through the configured stack of pre processors
+ * 
+ * @author mg@swiftly.org
+ */
+class PreProcessor
+{
+    /**
+     * Array of all configured pre processing steps.
+     * @var IPreProcessingStep[]
+     */
     private $preProcessingSteps;
 
-    public function __construct($modulesDirectory = null) {
+    /**
+     * Constructor for te PreProcessor
+     * @var string|null $modulesDirectory
+     */
+    public function __construct($modulesDirectory = null)
+    {
         $logger = \Swiftriver\Core\Setup::GetLogger();
+
         $logger->log("Core::PreProcessing::PreProcessor::__construct [Method invoked]", \PEAR_LOG_DEBUG);
         
         $logger->log("Core::PreProcessing::PreProcessor::__construct [START: Adding configured pre processors]", \PEAR_LOG_DEBUG);
@@ -16,15 +33,20 @@ class PreProcessor {
         $logger->log("Core::PreProcessing::PreProcessor::__construct [Method finished]", \PEAR_LOG_DEBUG);
     }
 
-    public function PreProcessContent($content) {
+    public function PreProcessContent($content)
+    {
         $logger = \Swiftriver\Core\Setup::GetLogger();
+
         $logger->log("Core::PreProcessing::PreProcessor::PreProcessContent [Method invoked]", \PEAR_LOG_DEBUG);
 
         $modulesDirectory = \Swiftriver\Core\Setup::Configuration()->ModulesDirectory;
+
         $configuration = \Swiftriver\Core\Setup::Configuration();
 
-        if(isset($this->preProcessingSteps) && count($this->preProcessingSteps) > 0) {
-            foreach($this->preProcessingSteps as $preProcessingStep) {
+        if(isset($this->preProcessingSteps) && count($this->preProcessingSteps) > 0)
+        {
+            foreach($this->preProcessingSteps as $preProcessingStep)
+            {
                 //Get the class name from config
                 $className = $preProcessingStep->className;
 
@@ -40,36 +62,42 @@ class PreProcessor {
 
                 $logger->log("Core::PreProcessing::PreProcessor::PreProcessContent [START: Instanciating pre processor: $className]", \PEAR_LOG_DEBUG);
 
-                try {
+                try
+                {
                     //Instanciate the pre processor
                     $preProcessor = new $className();
                 }
-                catch (\Exception $e) {
-                    $message = $e->getMessage();
-                    $logger->log("Core::PreProcessing::PreProcessor::PreProcessContent [$message]", \PEAR_LOG_ERR);
+                catch (\Exception $e)
+                {
+                    $logger->log("Core::PreProcessing::PreProcessor::PreProcessContent [$e]", \PEAR_LOG_ERR);
+                    
                     $logger->log("Core::PreProcessing::PreProcessor::PreProcessContent [Unable to run PreProcessing for preprocessor $className]", \PEAR_LOG_ERR);
+
+                    continue;
                 }
 
                 $logger->log("Core::PreProcessing::PreProcessor::PreProcessContent [END: Instanciating pre processor: $className]", \PEAR_LOG_DEBUG);
 
                 $logger->log("Core::PreProcessing::PreProcessor::PreProcessContent [START: Run PreProcessing for $className]", \PEAR_LOG_DEBUG);
 
-                try {
+                try
+                {
                     //Run the preocess method on the pre processor
                     $content = $preProcessor->Process($content, $configuration, $logger);
                 }
-                catch (\Exception $e) {
-                    $message = $e->getMessage();
-                    $logger->log("Core::PreProcessing::PreProcessor::PreProcessContent [$message]", \PEAR_LOG_ERR);
+                catch (\Exception $e)
+                {
+                    $logger->log("Core::PreProcessing::PreProcessor::PreProcessContent [$e]", \PEAR_LOG_ERR);
+
                     $logger->log("Core::PreProcessing::PreProcessor::PreProcessContent [Unable to run PreProcessing for preprocessor $className]", \PEAR_LOG_ERR);
                 }
 
                 $logger->log("Core::PreProcessing::PreProcessor::PreProcessContent [END: Run PreProcessing for $className]", \PEAR_LOG_DEBUG);
             }
-        } else {
-
+        } 
+        else
+        {
             $logger->log("Core::PreProcessing::PreProcessor::PreProcessContent [No PreProcessing Steps found to run]", \PEAR_LOG_DEBUG);
-
         }
 
         $logger->log("Core::PreProcessing::PreProcessor::PreProcessContent [Method finished]", \PEAR_LOG_DEBUG);
@@ -80,39 +108,61 @@ class PreProcessor {
 
     /**
      * Returns all the classes that implment the IPreProcessor interface
+     *
      * @return IPreProcessingStep[]
      */
-    public function ListAllAvailablePreProcessingSteps() {
+    public function ListAllAvailablePreProcessingSteps()
+    {
         $logger = \Swiftriver\Core\Setup::GetLogger();
+
         $logger->log("Core::PreProcessing::PreProcessor::ListAllAvailablePreProcessingSteps [Method invoked]", \PEAR_LOG_DEBUG);
 
         $steps = array();
 
         $modulesDirectory = \Swiftriver\Core\Setup::Configuration()->ModulesDirectory;
+
         $dirItterator = new \RecursiveDirectoryIterator($modulesDirectory);
+
         $iterator = new \RecursiveIteratorIterator($dirItterator, \RecursiveIteratorIterator::SELF_FIRST);
-        foreach($iterator as $file) {
-            if($file->isFile()) {
-                $filePath = $file->getPathname();
-                if(strpos($filePath, "PreProcessingStep.php")) {
-                    try {
-                        include_once($filePath);
-                        $typeString = "\\Swiftriver\\PreProcessingSteps\\".$file->getFilename();
-                        $type = str_replace(".php", "", $typeString);
-                        $object = new $type();
-                        if($object instanceof IPreProcessingStep) {
-                            $logger->log("Core::PreProcessing::PreProcessor::ListAllAvailablePreProcessingSteps [Adding type $type]", \PEAR_LOG_DEBUG);
-                            $object->filePath = str_replace($modulesDirectory, "", $filePath);
-                            $object->type = $type;
-                            $steps[] = $object;
-                        }
-                    }
-                    catch(\Exception $e) {
-                        $logger->log("Core::PreProcessing::PreProcessor::ListAllAvailablePreProcessingSteps [error adding type $type]", \PEAR_LOG_DEBUG);
-                        $logger->log("Core::PreProcessing::PreProcessor::ListAllAvailablePreProcessingSteps [$e]", \PEAR_LOG_ERR);
-                        continue;
-                    }
+        
+        foreach($iterator as $file)
+        {
+            if(!$file->isFile())
+                continue;
+            
+            $filePath = $file->getPathname();
+
+            if(!strpos($filePath, "PreProcessingStep.php"))
+                continue;
+
+            try
+            {
+                include_once($filePath);
+
+                $typeString = "\\Swiftriver\\PreProcessingSteps\\".$file->getFilename();
+
+                $type = str_replace(".php", "", $typeString);
+
+                $object = new $type();
+                
+                if($object instanceof IPreProcessingStep)
+                {
+                    $logger->log("Core::PreProcessing::PreProcessor::ListAllAvailablePreProcessingSteps [Adding type $type]", \PEAR_LOG_DEBUG);
+
+                    $object->filePath = str_replace($modulesDirectory, "", $filePath);
+
+                    $object->type = $type;
+
+                    $steps[] = $object;
                 }
+            }
+            catch(\Exception $e)
+            {
+                $logger->log("Core::PreProcessing::PreProcessor::ListAllAvailablePreProcessingSteps [error adding type $type]", \PEAR_LOG_DEBUG);
+
+                $logger->log("Core::PreProcessing::PreProcessor::ListAllAvailablePreProcessingSteps [$e]", \PEAR_LOG_ERR);
+
+                continue;
             }
         }
 
