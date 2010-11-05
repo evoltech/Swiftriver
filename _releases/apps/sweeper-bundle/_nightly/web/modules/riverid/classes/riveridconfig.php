bbfa3c2e5ceab89c53aebf8924e08768178d1b8f
Matthew Kidza-Griffiths <mrmatthewgriffiths@gmail.com>
2010-11-05 14:25:16 +0300

    Local install of the nightly build

--- _releases/apps/sweeper-bundle/_nightly/web/modules/riverid/classes/riveridconfig.php
+++ _releases/apps/sweeper-bundle/_nightly/web/modules/riverid/classes/riveridconfig.php
@@ -2,9 +2,9 @@
 class RiverIdConfig
 {
     public static $databaseurl = 'localhost';
-    public static $username = 'sweeper_test';
-    public static $password = 'sweeper_test';
-    public static $database = 'sweeper_test';
+    public static $username = 'sweeper_nightly';
+    public static $password = 'sweeper_nightly';
+    public static $database = 'sweeper_nightly';
 
     public static $createsql = "CREATE TABLE IF NOT EXISTS users ( username VARCHAR(2000), password VARCHAR(2000), role VARCHAR(2000) ) TYPE=innodb";
 }
--- _releases/apps/sweeper-bundle/_nightly/web/modules/swiftrivertheming/classes/themingconfig.php
+++ _releases/apps/sweeper-bundle/_nightly/web/modules/swiftrivertheming/classes/themingconfig.php
@@ -2,9 +2,9 @@
 class ThemingConfig
 {
     public static $databaseurl = 'localhost';
-    public static $username = 'sweeper_test';
-    public static $password = 'sweeper_test';
-    public static $database = 'sweeper_test';
+    public static $username = 'sweeper_nightly';
+    public static $password = 'sweeper_nightly';
+    public static $database = 'sweeper_nightly';
 
     public static $createsql = "CREATE TABLE IF NOT EXISTS theming ( theme VARCHAR(2000) ) TYPE=innodb";
 }
